40463ab5dded9b1f50edfc1cb1f88a975ac1d454
Songjie Yin <61562439+Kelvin0123@users.noreply.github.com>
2021-02-21 17:05:38 -0500

    HW4

--- assignment_04/my_A4_functions.py
+++ assignment_04/my_A4_functions.py
@@ -24,6 +24,7 @@
 # import name_of_module
 
 import numpy
+import math
 import doctest
 
 ##################################################
@@ -63,24 +64,66 @@ def matrix_multiple(mat_1: numpy.ndarray,mat_2: numpy.ndarray) ->numpy.ndarray:
 def ssr_loops(y: list,x: list,beta_0: float,beta_1: float) -> float:
     """ Calculates the value of the sum of squared residuals
         of the regression model.
+        Precondition: length of y must equal length of x.
         
     >>> ssr_loops([1,2,3],[4,5,6],7.0,8.0)
+    6173.0
+    >>> ssr_loops([2.0,0.0,-1.0],[10.0,-5.0,2.0],1.0,2.0)
+    478.0
+    >>> ssr_loops([2],[3,4],5,6)
+    None
     """
-    ssr=0
-    for i in range(len(x)):
-            ssr = sum((y[i]-beta_0-beta_1*x[i])**2)
+    ssr=0   
+    if len(x) != len(y):
+        return None
+    else: 
+        for i in range(len(x)):
+            ssr += ((y[i]-beta_0-beta_1*x[i])**2)
     return ssr
-
-# Exercise 3
-def ssr_vec(y,x,beta_0,beta_1):
     
-    return
-
+# Exercise 3
+def ssr_vec(y: list,x: list,beta_0: float,beta_1: float) -> float:
+    """Calculates the value of the sum of squared residuals
+        of the regression model.
+        Precondition: length of y must equal length of x.
+        
+    >>> ssr_vec(numpy.array([[1,2,3]]),numpy.array([[4,5,6]]),7.0,8.0)
+    6173.0
+    >>> ssr_vec(numpy.array([[2.0,0.0,-1.0]]),numpy.array([[10.0,-5.0,2.0]]),1.0,2.0)
+    478.0
+    >>> ssr_vec(numpy.array([[2]]),numpy.array([[3,4]]),5,6)
+    None
+    """
+    if len(x[0]) != len(y[0]):
+        return None
+    else:
+        ssr = ((y-beta_0-beta_1*x)**2) 
+    return numpy.sum ([ssr])
 
 # Exercise 4
-def logit_like_sum(y,x,beta_0,beta_1):
-    return
+def logit_like_sum(y: list,x: list,beta_0: float,beta_1: float) -> float:
+    """Return the value of the logit link function when yi=1,or 
+    return 1-logit link function when yi=0.
 
+    >>> logit_like_sum([1,0],[13,0], 0, 0)
+    -1.3862943611198906
+    >>> logit_like_sum([0,1,0,1,1], [0.0,12.5,2.6,1,3], math.log(2), 2.0)
+    -7.061229058181034
+    >>> logit_like_sum([1,3], [1.0,12], 0.0, math.log(5))
+    None
+    """
+    like = 0
+    
+    for i in range(len(y)):
+        if y[i] == 0:
+            like += math.log(1 - (math.exp(beta_0 + x[i]*beta_1)/(1 + math.exp(beta_0 + x[i]*beta_1))))
+        elif y[i] == 1:
+            like += math.log(math.exp(beta_0 + x[i]*beta_1)/(1 + math.exp(beta_0 + x[i]*beta_1)))
+        else:
+            print("Warning: y is not binary. y should be either 1 or 0.")
+            like = None
+        
+    return like
 
 # Only function definitions above this point. 
 
