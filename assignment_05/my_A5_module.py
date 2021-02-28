1a24c0b128a0a0bcb9b745f6d42fb597aed7d147
Songjie Yin <61562439+Kelvin0123@users.noreply.github.com>
2021-02-27 20:00:19 -0500

    Update my_A5_module.py

--- assignment_05/my_A5_module.py
+++ assignment_05/my_A5_module.py
@@ -59,7 +59,7 @@ def variance(x: list) -> float:
     9.949795918367347
     >>> variance([0,0,0,0,0,0,0,0,0])
     0.0
-    
+
     """
     var = 0
     diff = 0
@@ -86,6 +86,7 @@ def covariance(y: list,x: list) -> float:
     116.88
     >>> covariance([0,1,2,3],[4,5])
     None
+    
     """
     diff=0
     
@@ -110,9 +111,10 @@ def ols_slope(y: list,x: list) -> float:
     
     >>> ols_slope([1,2,3],[4,5,6])
     1.0
-    >>>
-    >>>
-    
+    >>> ols_slope([2,4,6,8,10],[1,3,5,7,9])
+    1.0
+    >>> ols_slope()
+    0.49165020650026936
     """
     cov = 0
     var = 0    
@@ -122,8 +124,8 @@ def ols_slope(y: list,x: list) -> float:
     for i in range(len(y)):
         cov += (y[i] - y_bar)*(x[i] - x_bar)
     
-    for i in range(len(x)):
-        var += (x[i] - x_bar)**2
+    for j in range(len(x)):
+        var += (x[j] - x_bar)**2
     
     beta_1_hat = cov/var
     
@@ -134,11 +136,12 @@ def ols_intercept(y: list,x: list,beta_1_hat: float) -> float:
     """ Calculates the intercept co-efficient for the linear
     regression model.
     
-    >>> ols_intercept([1,2,3],[4,5,6],7)
-    -33.0
-    >>>
-    >>>
-    
+    >>> ols_intercept([1,2,3],[4,5,6],1)
+    -3.0
+    >>> ols_intercept([2,4,6,8,10],[1,3,5,7,9],1)
+    1.0
+    >>> ols_intercept([0,2.3,4,5.5],[2.2,2.5,3,10],0.49165020650026936)
+    0.7744478362363085
     """
     y_bar = sum(y)/len(y)
     x_bar = sum(x)/len(x)
@@ -154,11 +157,13 @@ def ssr(y: list,x: list,beta_0: float,beta_1: float) -> float:
     y and x are lists of equal length
     and beta_0 and eta_1 are scalar coefficients. 
     
-    >>> ssr(y = [2, 2, 2], x = [1, 1, 1], beta_0 = 0.5, beta_1 = 0.5)
+    >>> ssr([2,2,2],[1,1,1],0.5,0.5)
     3.0
-    >>> ssr(y = [3, 0, 3], x = [0, 2, 2], beta_0 = 1.0, beta_1 = 0.5)
+    >>> ssr([3,0,3],[0,2,2],1.0,0.5)
     9.0
-
+    >>> ssr([1,2,3],[4,5,6],7,8)
+    6173.0
+    
     """
     ssr = 0
     for i in range(len(y)):
@@ -172,11 +177,15 @@ def min_ssr(y, x, beta_0_min, beta_0_max, beta_1_min, beta_1_max) -> float:
     """ Calculates the sum of squared residuals for the linear
     regression model.
     
-    min_ssr([1,2,3,4],[4,5,6,50],0.5,100,0.5,6)
-    
+    >>> min_ssr([2,4,6,8,10],[1,3,5,7,9],-5,10,-5,10)
+    [1.0, 1.0]
+    >>> min_ssr([1,2,3],[4,5,6],-5,10,-5,10)
+    [-3.0, 1.0]
+    >>> min_ssr([0,2.3,4,5.5],[2.2,2.5,3,10],-5,5,-5,5)
+    [0.7744478362363085, 0.49165020650026936]
     """
-    beta_0_list = list(np.arange(beta_0_min,beta_0_max,0.5))
-    beta_1_list = list(np.arange(beta_0_min,beta_0_max,0.5))
+    beta_0_list = list(np.arange(beta_0_min,beta_0_max,0.01))
+    beta_1_list = list(np.arange(beta_0_min,beta_0_max,0.01))
 
     min_ssr = 999999
     for i in range(len(beta_0_list)):
@@ -184,9 +193,11 @@ def min_ssr(y, x, beta_0_min, beta_0_max, beta_1_min, beta_1_max) -> float:
             ssr_i = ssr(y, x, beta_0_list[i], beta_1_list[j])
             if ssr_i < min_ssr:
                 min_ssr = ssr_i
-                beta = [i,j]
+                beta_0 = beta_0_list[i]
+                beta_1 = beta_1_list[j]
                 
-    return beta
+    return [beta_0,beta_1]
+
 
 # Only function definitions above this point. 
 
