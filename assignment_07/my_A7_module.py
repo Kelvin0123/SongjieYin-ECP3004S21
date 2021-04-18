c64a94627da5b00d6ec094b9a07739d0cc7f275b
Songjie Yin <61562439+Kelvin0123@users.noreply.github.com>
2021-04-18 16:01:25 -0400

    HW7

--- assignment_07/my_A7_module.py
+++ assignment_07/my_A7_module.py
@@ -4,7 +4,7 @@
 #
 # ECP 3004: Python for Business Analytics
 #
-# Name: 
+# Name: Songjie Yin
 #
 # Date:
 # 
@@ -49,30 +49,72 @@
 
 # Exercise 1
 
-def g(x):
-    """
+def g(x: float) -> float:
+    """ Calculates the value of the equation 
+    g(x)=(x-2)*x*(x+2)**2.
+    
+    >>> g(1.0) 
+    -9.0
+    >>> g(2.0)
+    0.0
+    >>> g(3.0)
+    75.0
     """
     
     return (x-2)*x*(x+2)**2
 
-def g_prime(x):
-    """
+def g_prime(x: float) -> float:
+    """ Calculates the first derivative of the
+    equation g(x)=(x-2)*x*(x+2)**2, with respect
+    to x.
+    
+    >>> g_prime(1.0)
+    -6.0
+    >>> g_prime(2.0)
+    32.0
+    >>> g_prime(3.0)
+    130.0
     """
     
-    return 2(x+2)(2*x**2-x-2)
+    return 2*(x+2)*(2*x**2-x-2)
 
-def g_2prime(x):
-    """
+def g_2prime(x: float) -> float:
+    """ Calculates the second derivative of the 
+    equation g(x)=(x-2)*x*(x+2)**2, with respect
+    to x.
+    
+    >>> g_2prime(1.0)
+    16.0
+    >>> g_2prime(2.0)
+    64.0
+    >>> g_2prime(3.0)
+    136.0
     """
     
     return 12*x**2+12*x-8
 
 # Exercise 2
 
-def newton_g_opt(x_0,maxiter,tol):
-    return
-
-
+def newton_g_opt(x_0: float,maxiter: int,tol: float) -> float:
+    """ Finds the minimum of g(x) using Newton's
+    method.
+    
+    >>> newton_g_opt(1.0,100,0.0001)
+    -9.91441130872133
+    >>> newton_g_opt(2.0,100,0.01)
+    -9.1875
+    >>> newton_g_opt(3.0,100,1)
+    1.4749109224775752
+    """
+    x = x_0
+    for i in range(maxiter):
+        x_next = x - (g_prime(x)/g_2prime(x))
+        if (x_next - x) < tol:
+            return g(x_next)
+        x = x_next
+    if i == maxiter - 1 and x_next - x < tol:
+        
+        return g(x)
 
 # Only function definitions above this point. 
 
